3751bf33c1768536ca7ace49e947028d4ebd3ca8
Yist Lin <yishen992@gmail.com>
2020-03-27 20:18:02 +0800

    it is trainable now

--- modules/data.py
+++ modules/data.py
@@ -1,69 +1,36 @@
 """Dataset and DataLoader."""
 
-import pickle
-from functools import cached_property
+from random import randint
 
 import torch
 from torch.utils.data import Dataset
-from torch.nn.utils.rnn import pad_sequence
+# from torch.nn.utils.rnn import pad_sequence
 
 
-class VCTKDataset(Dataset):
-    """VCTK dataset."""
+class SpeakerUtterances(Dataset):
+    """Utterances of a single speaker."""
 
-    def __init__(self, pickle_path):
+    def __init__(self, utterances, min_len=64, max_len=128):
         """
         Args:
-            pickle_path (string): Path to the pickle file.
+            utterances (list): List of numpy.ndarray
         """
-        self.file_path = pickle_path
-        self.raw_data = self._load_data()
-        self.data, self.idxs = self._tensorize_n_flatten_data()
-
-    @cached_property
-    def n_speakers(self):
-        """Get the number of speakers."""
-        return len(self.raw_data)
-
-    @cached_property
-    def n_utterances(self):
-        """Get the number of utterances per speaker."""
-        return len(self.raw_data[0])
+        self.raw_data = utterances
+        self.data = self._to_cuda_tensor()
+        self.min_len = min_len
+        self.max_len = max_len
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        return self.data[idx], self.idxs[idx]
+        uttr = self.data[idx]
 
-    def _load_data(self):
-        with open(self.file_path, 'rb') as data_file:
-            raw_data = pickle.load(data_file)
-        return raw_data
+        if len(uttr) <= self.max_len:
+            return uttr
 
-    def _tensorize_n_flatten_data(self):
-        data = [uttr for spkr in self.raw_data for uttr in spkr]
-        data = [torch.from_numpy(uttr).cuda() for uttr in data]
+        l_bound = randint(0, len(uttr) - self.max_len)
+        return uttr[l_bound:l_bound+self.max_len, :]
 
-        idxs = torch.arange(self.n_speakers).unsqueeze(1)
-        idxs = idxs.expand(self.n_speakers, self.n_utterances).flatten()
-
-        lens = [len(uttr) for uttr in data]
-
-        combined = list(zip(data, idxs, lens))
-        combined = sorted(combined, key=lambda x: x[2])
-
-        data, idxs, _ = zip(*combined)
-
-        return list(data), list(idxs)
-
-
-def pad_batch(batch):
-    """Pad a batch of sequences."""
-
-    data, idxs = zip(*batch)
-
-    lens = [len(uttr) for uttr in data]
-    data_pad = pad_sequence(data, batch_first=True, padding_value=0)
-
-    return data_pad, list(idxs), lens
+    def _to_cuda_tensor(self):
+        return [torch.from_numpy(uttr).cuda() for uttr in self.raw_data]
--- train.py
+++ train.py
@@ -3,55 +3,61 @@
 """Train d-vector."""
 
 import argparse
+import pickle
+import random
 
+import tqdm
 import torch
 from torch.optim import SGD
-from torch.utils.data import DataLoader
-from tqdm import trange
+from torch.utils.data import RandomSampler
+from torch.nn.utils.rnn import pad_sequence
 
-from modules.data import VCTKDataset, pad_batch
+from tensorboardX import SummaryWriter
+
+from modules.data import SpeakerUtterances
 from modules.dvector import DVector
 from modules.ge2e import GE2ELoss
 
 
-def train(data_path, model_path, save_every):
+def train(data_path, model_path,
+          save_every, n_steps, n_speakers, n_utterances):
     """Train a d-vector network."""
 
-    data = VCTKDataset(data_path)
-    data_loader = DataLoader(data, batch_size=32, collate_fn=pad_batch)
+    with open(data_path, 'rb') as data_file:
+        data = pickle.load(data_file)
+
+    assert n_speakers <= len(data)
+
+    datasets = [SpeakerUtterances(datum) for datum in data]
+    samplers = [RandomSampler(dts, replacement=True, num_samples=n_utterances)
+                for dts in datasets]
+
     dvector = DVector(num_layers=3, dim_input=80,
-                      dim_cell=768, dim_emb=256).cuda()
+                      dim_cell=256, dim_emb=128).cuda()
     optimizer = SGD(dvector.parameters(), lr=0.01)
-    criterion = GE2ELoss(init_w=10.0, init_b=-5.0, loss_method='softmax')
+    criterion = GE2ELoss(init_w=10.0, init_b=-5.0,
+                         loss_method='softmax').cuda()
+    writer = SummaryWriter()
 
-    pbar = trange(1000)
-    for epoch in pbar:
+    pbar = tqdm.trange(n_steps)
+    for step in pbar:
 
-        loader_iter = iter(data_loader)
-        embd_cnt = [0] * data.n_speakers
-        embd_set = torch.empty(data.n_speakers, data.n_utterances, 256)
+        sids = random.sample(range(len(data)), k=n_speakers)
+        samples = [datasets[sid][uid] for sid in sids for uid in samplers[sid]]
+        padded = pad_sequence(samples, batch_first=True, padding_value=0)
 
-        for padded, sids, _ in loader_iter:
+        embd = dvector(padded).view(n_speakers, n_utterances, -1)
 
-            print(padded.shape)
-
-            embd = dvector(padded)
-
-            for idx, sid in enumerate(sids):
-                embd_set[sid, embd_cnt[sid], :] = embd[idx]
-                embd_cnt[sid] += 1
-
-        break
-
-        loss = criterion(embd_set)
+        loss = criterion(embd)
 
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        pbar.set_description(f"loss = {loss:.4f}")
+        pbar.set_description(f"loss = {loss:.2f}")
+        writer.add_scalar('GE2E Loss', loss, step)
 
-        if (epoch + 1) % save_every == 0:
+        if (step + 1) % save_every == 0:
             torch.save(dvector.state_dict(), model_path)
 
 
@@ -63,8 +69,14 @@ def parse_args():
                         help="data path of utterances")
     parser.add_argument("model_path", type=str,
                         help="path to save model")
-    parser.add_argument("-s", "--save_every", type=int, default=1000,
+    parser.add_argument("-s", "--save_every", type=int, default=10000,
                         help="save model every [save_every] epochs")
+    parser.add_argument("-i", "--n_steps", type=int, default=1000000,
+                        help="# of steps")
+    parser.add_argument("-n", "--n_speakers", type=int, default=32,
+                        help="# of speakers per batch")
+    parser.add_argument("-m", "--n_utterances", type=int, default=5,
+                        help="# of utterances per speaker")
 
     return parser.parse_args()
 

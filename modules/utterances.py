5e47d5f6e72c99fbd81923da2a611ae87d439032
Yist Lin <yishen992@gmail.com>
2020-03-28 23:35:06 +0800

    save checkpoints during training

--- modules/utterances.py
+++ modules/utterances.py
@@ -1,5 +1,6 @@
 """Dataset and DataLoader."""
 
+import os
 import pickle
 from random import randint, sample
 
@@ -11,16 +12,24 @@ from torch.nn.utils.rnn import pad_sequence
 class Utterances(Dataset):
     """Utterances of a single speaker."""
 
-    def __init__(self, data_path, n_utterances=5, seg_len=128):
+    def __init__(self, data_dir, n_utterances=5, seg_len=128):
         """Args:
-            data_path (list): path to pickle file.
+            data_dir (list): path to the directory of pickle files.
         """
-        with open(data_path, 'rb') as data_file:
-            data = pickle.load(data_file)
 
-        self.data = [[torch.from_numpy(u).cuda() for u in s] for s in data]
-        self.seg_len = seg_len
+        assert os.path.isdir(data_dir)
+
+        self.data = []
         self.n_uttrances = n_utterances
+        self.seg_len = seg_len
+
+        for data_file in os.listdir(data_dir):
+            data_path = os.path.join(data_dir, data_file)
+            raw = pickle.load(open(data_path, "rb"))
+            data = [torch.from_numpy(d) for d in raw if len(d) > seg_len]
+            if len(data) < n_utterances:
+                continue
+            self.data.append(data)
 
     def __len__(self):
         return len(self.data)
--- train.py
+++ train.py
@@ -4,9 +4,11 @@
 
 import argparse
 
+import os
 import tqdm
 import torch
 from torch.optim import SGD
+from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader
 
 from tensorboardX import SummaryWriter
@@ -16,68 +18,117 @@ from modules.dvector import DVector
 from modules.ge2e import GE2ELoss
 
 
-def train(data_path, model_path,
-          save_every, n_steps, n_speakers, n_utterances):
-    """Train a d-vector network."""
-
-    dataset = Utterances(data_path, n_utterances=n_utterances, seg_len=128)
-    loader = DataLoader(dataset, batch_size=n_speakers, shuffle=True,
-                        collate_fn=pad_batch, drop_last=True)
-
-    assert n_speakers <= len(dataset)
-
-    dvector = DVector(num_layers=2, dim_input=80,
-                      dim_cell=256, dim_emb=128).cuda()
-    optimizer = SGD(dvector.parameters(), lr=0.01)
-    criterion = GE2ELoss(init_w=10.0, init_b=-5.0,
-                         loss_method='softmax').cuda()
-    writer = SummaryWriter()
-
-    loader_iter = iter(loader)
-
-    pbar = tqdm.trange(n_steps)
-    for step in pbar:
-
-        try:
-            batch = next(loader_iter)
-        except StopIteration:
-            loader_iter = iter(loader)
-            batch = next(loader_iter)
-
-        embd = dvector(batch).view(n_speakers, n_utterances, -1)
-
-        loss = criterion(embd)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        pbar.set_description(f"loss = {loss:.2f}")
-        writer.add_scalar('GE2E Loss', loss, step)
-
-        if (step + 1) % save_every == 0:
-            torch.save(dvector.state_dict(), model_path)
-
-
 def parse_args():
     """Parse command-line arguments."""
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("data_path", type=str,
-                        help="data path of utterances")
-    parser.add_argument("model_path", type=str,
-                        help="path to save model")
+    parser.add_argument("data_dir", type=str,
+                        help="path to directory of processed utterances")
+    parser.add_argument("model_dir", type=str,
+                        help="path to directory for saving checkpoints")
+    parser.add_argument("-c", "--checkpoint_path", type=str, default=None,
+                        help="path to load saved checkpoint")
+    parser.add_argument("-i", "--n_steps", type=int, default=1000000,
+                        help="total # of steps")
     parser.add_argument("-s", "--save_every", type=int, default=10000,
                         help="save model every [save_every] epochs")
-    parser.add_argument("-i", "--n_steps", type=int, default=3000000,
-                        help="# of steps")
+    parser.add_argument("-d", "--decay_every", type=int, default=100000,
+                        help="decay learning rate every [decay_every] steps")
     parser.add_argument("-n", "--n_speakers", type=int, default=32,
                         help="# of speakers per batch")
     parser.add_argument("-m", "--n_utterances", type=int, default=5,
                         help="# of utterances per speaker")
+    parser.add_argument("-l", "--seg_len", type=int, default=128,
+                        help="length of the segment of an utterance")
 
     return parser.parse_args()
 
 
+class TrainingManager:
+    """Manage the training process."""
+
+    def __init__(self, data_dir, model_dir, checkpoint_path,
+                 n_steps, save_every, decay_every,
+                 n_speakers, n_utterances, seg_len):
+
+        self.n_lstms = 3
+        self.d_input = 80
+        self.d_cell = 256
+        self.d_emb = 128
+        self.steps = 0
+
+        writer = SummaryWriter()
+        dataset = Utterances(data_dir, n_utterances, seg_len)
+        loader = DataLoader(dataset, batch_size=n_speakers, shuffle=True,
+                            collate_fn=pad_batch, drop_last=True)
+
+        print(f"Training starts with {len(dataset)} speakers.")
+
+        ckpt = None
+        if checkpoint_path is not None:
+            ckpt = torch.load(checkpoint_path)
+            self.__dict__.update(ckpt["manager"])
+
+        dvector = DVector(self.n_lstms, self.d_input, self.d_cell, self.d_emb)
+        criterion = GE2ELoss()
+        optimizer = SGD(dvector.parameters(), lr=0.01)
+        scheduler = StepLR(optimizer, step_size=decay_every, gamma=0.5)
+
+        if ckpt is not None:
+            dvector.load_state_dict(ckpt["state_dict"])
+            criterion.load_state_dict(ckpt["criterion"])
+            optimizer.load_state_dict(ckpt["optimizer"])
+            scheduler.load_state_dict(ckpt["scheduler"])
+
+        self.train(n_steps, save_every, model_dir, n_speakers, n_utterances,
+                   loader, writer, dvector, criterion, optimizer, scheduler)
+
+    def train(self, n_steps, save_every, model_dir, n_speakers, n_utterances,
+              loader, writer, dvector, criterion, optimizer, scheduler):
+        """Train the network."""
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        dvector = dvector.to(device)
+        criterion = criterion.to(device)
+
+        loader_iter = iter(loader)
+        pbar = tqdm.trange(n_steps)
+
+        for step in pbar:
+
+            self.steps += 1
+
+            try:
+                batch = next(loader_iter)
+            except StopIteration:
+                loader_iter = iter(loader)
+                batch = next(loader_iter)
+
+            embd = dvector(batch.to(device)).view(n_speakers, n_utterances, -1)
+
+            loss = criterion(embd)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            pbar.set_description(f"global = {self.steps}, loss = {loss:.4f}")
+            writer.add_scalar("GE2E Loss", loss, self.steps)
+
+            if (step + 1) % save_every == 0:
+                ckpt_path = os.path.join(model_dir, f"ckpt-{self.steps}.tar")
+                ckpt_dict = {
+                    "manager": self.__dict__,
+                    "state_dict": dvector.state_dict(),
+                    "criterion": criterion.state_dict(),
+                    "optimizer": optimizer.state_dict(),
+                    "scheduler": scheduler.state_dict(),
+                }
+                torch.save(ckpt_dict, ckpt_path)
+
+        print("Training completed.")
+
+
 if __name__ == "__main__":
-    train(**vars(parse_args()))
+    TrainingManager(**vars(parse_args()))

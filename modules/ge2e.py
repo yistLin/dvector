62871543fc0c90b4f22e5510b27a2c110bcec3ce
Yist Lin <yishen992@gmail.com>
2020-04-01 22:43:07 +0800

    speed up calculation of cosine similarity

--- modules/ge2e.py
+++ modules/ge2e.py
@@ -1,24 +1,24 @@
+"""PyTorch implementation of GE2E loss"""
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class GE2ELoss(nn.Module):
+    """Implementation of the GE2E loss in https://arxiv.org/abs/1710.10467 [1]
+
+    Accepts an input of size (N, M, D)
+
+        where N is the number of speakers in the batch,
+        M is the number of utterances per speaker,
+        and D is the dimensionality of the embedding vector (e.g. d-vector)
+
+    Args:
+        - init_w (float): the initial value of w in Equation (5) of [1]
+        - init_b (float): the initial value of b in Equation (5) of [1]
+    """
 
     def __init__(self, init_w=10.0, init_b=-5.0, loss_method='softmax'):
-        '''
-        Implementation of the Generalized End-to-End loss defined in https://arxiv.org/abs/1710.10467 [1]
-
-        Accepts an input of size (N, M, D)
-
-            where N is the number of speakers in the batch,
-            M is the number of utterances per speaker,
-            and D is the dimensionality of the embedding vector (e.g. d-vector)
-
-        Args:
-            - init_w (float): defines the initial value of w in Equation (5) of [1]
-            - init_b (float): definies the initial value of b in Equation (5) of [1]
-        '''
         super(GE2ELoss, self).__init__()
         self.w = nn.Parameter(torch.tensor(init_w))
         self.b = nn.Parameter(torch.tensor(init_b))
@@ -31,41 +31,32 @@ class GE2ELoss(nn.Module):
         if self.loss_method == 'contrast':
             self.embed_loss = self.embed_loss_contrast
 
-    def calc_new_centroids(self, dvecs, centroids, spkr, utt):
-        '''
-        Calculates the new centroids excluding the reference utterance
-        '''
-        excl = torch.cat((dvecs[spkr, :utt], dvecs[spkr, utt+1:]))
-        excl = torch.mean(excl, 0)
-        new_centroids = []
-        for i, centroid in enumerate(centroids):
-            if i == spkr:
-                new_centroids.append(excl)
-            else:
-                new_centroids.append(centroid)
-        return torch.stack(new_centroids)
+    def cosine_similarity(self, dvecs):
+        """Calculate cosine similarity matrix of shape (N, M, N)."""
+        n_spkr, n_uttr, d_embd = dvecs.size()
 
-    def calc_cosine_sim(self, dvecs, centroids):
-        '''
-        Make the cosine similarity matrix with dims (N,M,N)
-        '''
-        cos_sim_matrix = []
-        for spkr_idx, speaker in enumerate(dvecs):
-            cs_row = []
-            for utt_idx, utterance in enumerate(speaker):
-                new_centroids = self.calc_new_centroids(
-                    dvecs, centroids, spkr_idx, utt_idx)
-                # vector based cosine similarity for speed
-                cs_row.append(torch.clamp(torch.mm(utterance.unsqueeze(1).transpose(0, 1), new_centroids.transpose(
-                    0, 1)) / (torch.norm(utterance) * torch.norm(new_centroids, dim=1)), 1e-6))
-            cs_row = torch.cat(cs_row, dim=0)
-            cos_sim_matrix.append(cs_row)
-        return torch.stack(cos_sim_matrix)
+        dvec_expns = dvecs.unsqueeze(-1).expand(n_spkr, n_uttr, d_embd, n_spkr)
+        dvec_expns = dvec_expns.transpose(2, 3)
+
+        ctrds = dvecs.mean(dim=1)
+        ctrd_expns = ctrds.unsqueeze(0).expand(n_spkr * n_uttr, n_spkr, d_embd)
+        ctrd_expns = ctrd_expns.view_as(dvec_expns)
+
+        dvec_rolls = torch.cat([dvecs[:, 1:, :], dvecs[:, :-1, :]], dim=1)
+        dvec_excls = dvec_rolls.unfold(1, n_uttr-1, 1)
+        mean_excls = dvec_excls.mean(dim=-1)
+
+        ctrd_excls = torch.stack([
+            torch.cat([ctrds[:s, :],
+                       mean_excls[s, u, :].unsqueeze(0),
+                       ctrds[s+1:, :]])
+            for s in range(n_spkr) for u in range(n_uttr)
+        ]).view_as(dvec_expns)
+
+        return F.cosine_similarity(dvec_expns, ctrd_excls, 3, 1e-6)
 
     def embed_loss_softmax(self, dvecs, cos_sim_matrix):
-        '''
-        Calculates the loss on each embedding $L(e_{ji})$ by taking softmax
-        '''
+        """Calculate the loss on each embedding by taking softmax."""
         N, M, _ = dvecs.shape
         L = []
         for j in range(N):
@@ -77,9 +68,7 @@ class GE2ELoss(nn.Module):
         return torch.stack(L)
 
     def embed_loss_contrast(self, dvecs, cos_sim_matrix):
-        '''
-        Calculates the loss on each embedding $L(e_{ji})$ by contrast loss with closest centroid
-        '''
+        """Calculate the loss on each embedding by contrast loss."""
         N, M, _ = dvecs.shape
         L = []
         for j in range(N):
@@ -95,14 +84,8 @@ class GE2ELoss(nn.Module):
         return torch.stack(L)
 
     def forward(self, dvecs):
-        '''
-        Calculates the GE2E loss for an input of dimensions (num_speakers, num_utts_per_speaker, dvec_feats)
-        '''
-        # Calculate centroids
-        centroids = torch.mean(dvecs, 1)
-
-        # Calculate the cosine similarity matrix
-        cos_sim_matrix = self.calc_cosine_sim(dvecs, centroids)
+        """Calculate the GE2E loss for an input of dimensions (N, M, D)."""
+        cos_sim_matrix = self.cosine_similarity(dvecs)
         torch.clamp(self.w, 1e-6)
         cos_sim_matrix = cos_sim_matrix * self.w + self.b
         L = self.embed_loss(dvecs, cos_sim_matrix)

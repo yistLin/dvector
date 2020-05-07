0f2c8f68f416ab70a6faf568b8f1cb887ac23e94
Yist Lin <yishen992@gmail.com>
2020-05-07 12:04:19 +0800

    save in npy instead of pkl

--- prepare.py
+++ prepare.py
@@ -2,57 +2,61 @@
 # -*- coding: utf-8 -*-
 """Preprocess audio files."""
 
-import os
-import pickle
-import argparse
+from argparse import ArgumentParser
 from multiprocessing import Pool
+from os import listdir, makedirs
+from os.path import basename, isdir, join as join_path, splitext
 
-from modules.dirswalker import DirsWalker
+from numpy import save as save_npy
+from librosa.util import find_files as find_audio_files
+
 from modules.audioprocessor import AudioProcessor
 
 
-def prepare(root_paths, save_dir, extensions, n_threads):
+def prepare(root_paths, save_dir, n_threads):
     """Extract audio files from directories and turn into spectrograms."""
 
-    assert os.path.isdir(save_dir)
+    assert isdir(save_dir)
 
     n_speakers = 0
 
     for root_path in root_paths:
 
-        walker = DirsWalker(root_path, extensions)
+        spkr_ids = [entry for entry in listdir(root_path)
+                    if isdir(join_path(root_path, entry))]
+        spkr_paths = [join_path(root_path, spkr) for spkr in spkr_ids]
 
-        for sdir in walker:
+        for spkr_id, spkr_path in zip(spkr_ids, spkr_paths):
 
-            paths = list(sdir)
+            uttr_paths = find_audio_files(spkr_path)
+            uttr_ids = [splitext(basename(u))[0] for u in uttr_paths]
 
-            print(f"Collecting {len(paths):4d} utterances from {sdir.path}")
+            print(f"Collecting {len(uttr_paths)} utterances from {spkr_path}")
 
-            if len(paths) == 0:
+            if len(uttr_paths) == 0:
                 continue
 
             n_speakers += 1
 
+            specs_path = join_path(save_dir, f"s{n_speakers:04d}({spkr_id})")
+
+            makedirs(specs_path)
+
             with Pool(n_threads) as pool:
-                specs = pool.map(AudioProcessor.file2spectrogram, paths)
+                specs = pool.map(AudioProcessor.file2spectrogram, uttr_paths)
 
-            save_path = os.path.join(
-                save_dir, f"s{n_speakers:04d}({sdir.name}).pkl")
-
-            with open(save_path, 'wb') as out_file:
-                pickle.dump(specs, out_file)
+            for spec, uttr_id in zip(specs, uttr_ids):
+                save_npy(join_path(specs_path, uttr_id), spec)
 
 
 def parse_args():
     """Parse command-line arguments."""
 
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
     parser.add_argument("root_paths", nargs='+',
                         help="root directory of directories of speakers")
     parser.add_argument("-s", "--save_dir", type=str, required=True,
                         help="path to the directory to save processed object")
-    parser.add_argument("-e", "--extensions", type=str, default="wav",
-                        help="file extensions to use")
     parser.add_argument("-t", "--n_threads", type=int, default=4,
                         help="# of threads to use")
 
